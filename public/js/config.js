b6aad5aae338267db3881362b398cc337b8e7533
Hamzah Fauzi <muhammadf@thebigrichgroup.com>
2019-12-13 15:34:15 +0700

    add production branch

--- public/js/config.js
+++ public/js/config.js
@@ -1,9 +1,9 @@
 var config = {
 	getApiUrl: () => {
-		return 'http://eplanning.asahankab.go.id/simulasiv2/eperencanaan/web/index.php?r=';
+		return 'http://eplanning.asahankab.go.id/2020/eperencanaan/web/index.php?r=';
 	},
 	getMediaUrl: () => {
-		return 'http://eplanning.asahankab.go.id/simulasiv2/eperencanaan/web/uploads';
+		return 'http://eplanning.asahankab.go.id/2020/eperencanaan/web/uploads';
 	},
 	getRoleName: () => {
 		return 'Operator_Kecamatan'
@@ -21,7 +21,8 @@ var config = {
 		return window.config.getApiUrl() + 'api/berita-acara&token=';
 	},
 	getEnv: () => {
-		return 'simulasi'
+		// return 'simulasi'
+		return 'production'
 	},
 	getAppName: () => {
 		return 'E-Musrenbang Kecamatan'
